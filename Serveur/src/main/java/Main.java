f6c36952c9043fb98d0bdbe6801b2c487f6bd78f
Guillaume Paoli <wdgaster.gtp@gmail.com>
2024-03-22 08:38:44 -0400

    choisir l'ip du serveur et son port
    Option en commentaire pour simplifier la prog

--- Serveur/src/main/java/Main.java
+++ Serveur/src/main/java/Main.java
@@ -1,2 +1,29 @@
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+        ServerSocket serverSocket;
+        Socket clientSocket;
+        InetAddress inetAddress = InetAddress.getByName("224.214.26.78");
+        int port = 8080;
+
+        //demander l'addresse et le port de connexion
+    //    Scanner scanner = new Scanner(System.in);
+    //    System.out.printf("IP : ");
+    //    scanner.nextLine();
+
+    //    inetAddress = InetAddress.getByName(scanner.toString());
+
+    //    System.out.printf("\nPort : ");
+    //    scanner.nextLine();
+
+    //    port = Integer.parseInt(scanner.toString());
+
+        //demarrer le serveur
+
+    }
 }
